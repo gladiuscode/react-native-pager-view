a1a26ddb004e1132d5282f120a4c95f5d6e3f67a
gladius <gladiusservices@protonmail.com>
2025-02-20 23:42:48 +0100

    feat: create horizontal carousel inside pager

--- example/src/BasicPagerViewExample.tsx
+++ example/src/BasicPagerViewExample.tsx
@@ -1,16 +1,42 @@
-import React, { useMemo } from 'react';
-import { StyleSheet, View, SafeAreaView, Animated, Text } from 'react-native';
+import React, {useCallback, useMemo} from 'react';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Animated,
+  Text,
+  FlatList,
+  useWindowDimensions,
+  ListRenderItem,
+} from 'react-native';
 
 import PagerView from 'react-native-pager-view';
 
-import { LikeCount } from './component/LikeCount';
 import { NavigationPanel } from './component/NavigationPanel';
 import { useNavigationPanel } from './hook/useNavigationPanel';
+
+const MESSAGES = [
+  'okay',
+  'sudo make me a sandwich',
+  'what? make it yourself',
+  'make me a sandwich',
+];
 
 const AnimatedPagerView = Animated.createAnimatedComponent(PagerView);
 
 export function BasicPagerViewExample() {
   const { ref, ...navigationPanel } = useNavigationPanel();
+  const { width } = useWindowDimensions();
+
+  const renderItem = useCallback<ListRenderItem<typeof MESSAGES[number]>>(({ item }) => {
+    return (
+      <View style={{ width, justifyContent: 'center', alignItems: 'center' }}>
+        <View accessible style={{ width: '60%', paddingVertical: 100, borderWidth: 1 }}>
+          <Text accessible={false}>{item}</Text>
+        </View>
+      </View>
+    );
+  }, [width]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,23 +47,21 @@ export function BasicPagerViewExample() {
         style={styles.PagerView}
         initialPage={0}
         pageMargin={10}
+        scrollEnabled={false}
       >
         {useMemo(
           () =>
-            navigationPanel.pages.map((page, index) => (
+            navigationPanel.pages.map((page) => (
               <View
                 testID="pager-view-content"
                 key={page.key}
                 style={page.style}
                 collapsable={false}
               >
-                <LikeCount />
-                <Text
-                  testID={`pageNumber${index}`}
-                >{`page number ${index}`}</Text>
+                <FlatList style={{ width }} data={MESSAGES} horizontal renderItem={renderItem} />
               </View>
             )),
-          [navigationPanel.pages]
+          [navigationPanel.pages, renderItem, width]
         )}
       </AnimatedPagerView>
       <NavigationPanel {...navigationPanel} />
